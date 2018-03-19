a52b005ccf6d060a82f65cbeb5b32bec841c9780
jephy <jephy@192.168.0.100>
2018-03-19 08:56:02 +0800

    add comment

--- src/main/java/linkedlist/MyLinkedList.java
+++ src/main/java/linkedlist/MyLinkedList.java
@@ -2,31 +2,47 @@ package linkedlist;
 
 /**
  * Created by jephy on 2018/3/18.
+ * 链表中最重要的几个概念应该就是结点、链头、链尾
+ * 链表的一个特点是"传递"。链头由head标记，而链尾的next指向为空。
+ * 通过head，以及next是否为空，可以很容易找到链尾。
+ *
+ * java实现链表的特点是引用当指针用
  */
 public class MyLinkedList {
 
     Node head = null;//链表头的引用
 
-    public void addNode(int d){
-        Node newNode = new Node(d);
-        if (head == null) {
+    /**
+     * 添加结点
+     * 从尾部添加
+     * 在可以将链表当作一个整体看待，
+     * @param data 数据
+     */
+    public void addNode(int data){
+        Node newNode = new Node(data);//先创建一个链结点
+        if (head == null) {//链表为空，即head指向为空的情况下，将新结点作为链表的头结点
             head = newNode;
             return;
         }
 
         Node tmp = head;
-        while (tmp.next != null) {
+        while (tmp.next != null) {//找到链尾的指针，该指针指向应该为null
             tmp = tmp.next;
         }
-        tmp.next = newNode;
+        tmp.next = newNode;//让链尾指针指向新创建的结点
     }
 
+    /**
+     *
+     * @param index 角标
+     * @return
+     */
     public boolean deleteNode(int index){
         if (index < 1 || index > length()) {
             return false;
         }
 
-        if (index == 1) {
+        if (index == 1) {//删除的是头结点
             head = head.next;
             return true;
         }
@@ -34,11 +50,13 @@ public class MyLinkedList {
         int i = 1;
         Node preNode = head;
         Node curNode = preNode.next;
-        while (curNode != null) {
-            if (i == index) {
+        while (curNode != null) {//移动到尾部没找到的话退出循环
+            if (i == index) {//循环移动是为了能够让引用指向指定角标的结点
                 preNode.next = curNode.next;
                 return true;
             }
+
+            //将2个指针整体向后移动指针一次
             preNode = curNode;
             curNode = curNode.next;
             i++;
@@ -46,6 +64,10 @@ public class MyLinkedList {
         return true;
     }
 
+    /**
+     * 用一个计数器数结点
+     * @return 返回链表的长度
+     */
     public int length() {
         int lenght = 0;
         Node tmp = head;
@@ -56,6 +78,10 @@ public class MyLinkedList {
         return lenght;
     }
 
+    /**
+     * 对链表进行排序，返回排序后的头结点
+     * @return
+     */
     public Node orderList(){
         Node nextNode = null;
         int temp = 0;
